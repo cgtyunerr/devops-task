4e94d417141f2af2f8ae5a33b32a99ab8a855cbf
cgtyunerr <cgtyuner06@gmail.com>
2025-05-08 21:40:04 +0300

    test(/): add client fixture and test health endpoint

--- conftest.py
+++ conftest.py
@@ -4,14 +4,23 @@ import os
 
 import pytest
 
+from fastapi.testclient import TestClient
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.inmemory import InMemoryBackend
+
+from app.api.app import app
 from tests import database
 
 
 @pytest.fixture(scope="module", autouse=True)
 def set_env_variable():
     os.environ["AIOCACHE_DISABLE"] = "1"
+
+
+@pytest.fixture(scope="session")
+def client():
+    with TestClient(app) as client:
+        yield client
 
 
 @pytest.fixture(autouse=True, scope="function")

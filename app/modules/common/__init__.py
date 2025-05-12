ec6f2785c25000cb5cd7f3ab0113b745e161066a
cgtyunerr <cgtyuner06@gmail.com>
2025-05-12 14:31:56 +0300

    feat(modules/common): add error handler middleware

--- app/modules/common/__init__.py
+++ app/modules/common/__init__.py
@@ -1,1 +1,17 @@
 """Common module."""
+
+from .exceptions import (
+    InvalidInputError,
+    NotFoundError,
+    ConflictError,
+    UnprocessableEntityError,
+)
+from .middlewares import ErrorHandlerMiddleware
+
+__all__ = [
+    "InvalidInputError",
+    "NotFoundError",
+    "ConflictError",
+    "UnprocessableEntityError",
+    "ErrorHandlerMiddleware",
+]
---
+++ app/modules/common/exceptions.py
@@ -0,0 +1,17 @@
+"""Common exceptions."""
+
+
+class InvalidInputError(ValueError):
+    """Invalid input."""
+
+
+class NotFoundError(ValueError):
+    """Not found."""
+
+
+class ConflictError(ValueError):
+    """Item conflict."""
+
+
+class UnprocessableEntityError(ValueError):
+    """Item is unprocessable."""
